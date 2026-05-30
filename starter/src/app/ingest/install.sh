279fcf0adf5a8f3a81b04f5c2377f6561a938eae
Marc Gueury <marc.gueury@oracle.com>
2026-05-30 18:00:15 +0200

    v3.0

--- starter/src/app/ingest/install.sh
+++ starter/src/app/ingest/install.sh
@@ -47,20 +47,20 @@ if [ "$TF_VAR_rag_storage" == "vector_store" ]; then
         source myenv/bin/activate
         python src/create_vector_store.py
         exit_on_error "create_vector_store.py failed"
+    fi
+fi  
 
-        . ./responses_env.sh
-        # Store the config in APEX
-        export TNS_ADMIN=$HOME/app/db
-        $HOME/app/db/sqlcl/bin/sql APEX_APP/$DB_PASSWORD@DB <<EOF
+# Store it in the Database
+. ./responses_env.sh
+# Store the config in APEX
+export TNS_ADMIN=$HOME/app/db
+HOME/app/db/sqlcl/bin/sql APEX_APP/$DB_PASSWORD@DB <<EOF
 begin
     AI_CONFIG_UPDATE( 'vector_store_id', '$VECTOR_STORE_ID' );
 end;
 /
 exit;
 EOF
-
-    fi
-fi    
 
 # Build Tika
 cd src/tika
