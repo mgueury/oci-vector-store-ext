132d3bdd760f452e3e11f2b1bfb2c9aa635de3b8
Marc Gueury <marc.gueury@oracle.com>
2026-06-09 15:52:34 +0200

    v3.0

--- starter/src/terraform/apigw.tf
+++ starter/src/terraform/apigw.tf
@@ -140,7 +140,7 @@ resource "oci_apigateway_deployment" "starter_apigw_deployment_chat3" {
       methods = [ "ANY" ]
       backend {
         type = "HTTP_BACKEND"
-        url    = "http://${local.apigw_dest_private_ip}:8082/api/$${request.path[pathname]}"
+        url    = "http://${local.apigw_dest_private_ip}:8082/$${request.path[pathname]}"
         connect_timeout_in_seconds = 60
         read_timeout_in_seconds = 120
         send_timeout_in_seconds = 120                    
@@ -149,3 +149,26 @@ resource "oci_apigateway_deployment" "starter_apigw_deployment_chat3" {
   }
   freeform_tags = local.api_tags
 }  
+
+resource "oci_apigateway_deployment" "starter_apigw_deployment_chat4" {
+  compartment_id = local.lz_app_cmp_ocid
+  display_name   = "${var.prefix}-chat4"
+  gateway_id     = local.apigw_ocid
+  path_prefix    = "/__nextjs_original-stack-frames"
+  specification {
+    # Route the COMPUTE_PRIVATE_IP   
+    routes {
+      path    = "/{pathname*}"
+      methods = [ "ANY" ]
+      backend {
+        type = "HTTP_BACKEND"
+        url    = "http://${local.apigw_dest_private_ip}:8082/__nextjs_original-stack-frames"
+        connect_timeout_in_seconds = 60
+        read_timeout_in_seconds = 120
+        send_timeout_in_seconds = 120                    
+      }
+    }      
+  }
+  freeform_tags = local.api_tags
+}  
+
