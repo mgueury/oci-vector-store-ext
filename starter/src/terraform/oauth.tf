91a29e04c50224db2abbce96e46f2fb81ae113d6
Marc Gueury <marc.gueury@oracle.com>
2026-06-12 00:38:29 +0200

    v3.0

---
+++ starter/src/terraform/oauth.tf
@@ -0,0 +1,61 @@
+locals {
+  openid_client_id = try( oci_identity_domains_app.starter_confidential_app[0].name, "" )
+  openid_client_secret = try( oci_identity_domains_app.starter_confidential_app[0].client_secret, "" )
+}
+
+variable "openid" {
+    default=""
+    description="Enable OpenID / OAuth2"
+}
+
+resource "oci_identity_domains_app" "starter_confidential_app" {
+  count = var.openid == "true" ? 1 : 0
+  active                  = "true"
+  all_url_schemes_allowed = "false"
+  allow_access_control    = "false"
+  allowed_grants = [
+    "client_credentials",
+    "authorization_code",
+  ]
+  allowed_operations = [
+    "introspect",
+  ]
+  attr_rendering_metadata {
+    name = "aliasApps"
+    section = ""
+    visible = "false"
+    widget  = ""
+  }
+  based_on_template {
+    value         = "CustomWebAppTemplateId"
+    well_known_id = "CustomWebAppTemplateId"
+  }
+  client_ip_checking = "anywhere"
+  client_type        = "confidential"
+  delegated_service_names = [
+  ]
+  display_name = "${var.prefix}-confidential-app"
+  idcs_endpoint = "${local.local_idcs_url}"
+  is_alias_app      = "false"
+  is_enterprise_app = "false"
+  is_kerberos_realm = "false"
+  is_login_target   = "true"
+  is_mobile_target  = "false"
+  is_oauth_client   = "true"
+  is_oauth_resource = "false"
+  is_saml_service_provider = "false"
+  is_unmanaged_app         = "false"
+  is_web_tier_policy       = "false"
+  login_mechanism = "OIDC"
+  post_logout_redirect_uris = [
+    "https://${local.apigw_hostname}/",
+  ]
+  redirect_uris = [
+    "https://${local.apigw_hostname}/"
+  ]
+  schemas = [
+    "urn:ietf:params:scim:schemas:oracle:idcs:App"
+  ]
+  show_in_my_apps = "false"
+  trust_scope     = "Account"
+}
