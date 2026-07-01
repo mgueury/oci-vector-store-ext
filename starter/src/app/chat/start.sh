d543c81ac4103f4b8f222b60d12b126a6ed0373e
Marc Gueury <marcgueury@Marcs-MacBook-Pro.local>
2026-07-01 16:31:58 +0200

    v3.0

--- starter/src/app/chat/start.sh
+++ starter/src/app/chat/start.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
+
 . ../ingest/env.sh
 
 # OCI Enterprise AI: set to your tenancy's region
