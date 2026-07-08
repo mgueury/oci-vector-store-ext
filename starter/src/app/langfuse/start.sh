552771b663ab455cf95e21b0aba3a5a50d5da64b
Marc Gueury <marcgueury@Marcs-MacBook-Pro.local>
2026-07-08 15:04:59 +0200

    v3.0

--- starter/src/app/langfuse/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd $SCRIPT_DIR
-export PATH=~/.local/bin/:$PATH
-
-. $HOME/compute/tf_env.sh
-
-cd langfuse
-podman-compose up 2>&1 | tee langfuse.log
